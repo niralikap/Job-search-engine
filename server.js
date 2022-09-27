cdca4e92b2822dcdc5e07b4d2bf0e299a5656b84
YeshNels <paleojosh@sbcglobal.net>
2022-09-26 23:00:24 -0700

    Changes server file to get form pulled up.

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const session = require('express-session');
 const routes = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'lock&key',
@@ -32,7 +34,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
