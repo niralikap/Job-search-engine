653688479fc3f42e7ddabc142693b8285b73df09
YeshNels <paleojosh@sbcglobal.net>
2022-09-26 20:17:03 -0700

    Updated seed data for testing. Adds elementary steps to server file.

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const session = require('express-session');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'passcode goes here later',
+    secret: 'lock&key',
     cookie: {
-        maxAge: 360000,
+        maxAge: 3600000,
         httpOnly: true,
         secure: false,
         sameSite: 'strict',
@@ -22,3 +26,16 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
