7f7141f3ea6cdbe38cdc3dc6025f4b243ebaff50
Nilu <nirali.rkapadia@gmail.com>
2022-09-28 05:57:51 -0700

    Adds the login page handle bar files along with the login and logout JS files with the code

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -39,31 +39,52 @@ router.post('/', async (req, res) => {
   }
 });
 
-module.exports = router;
 
-// Added comments describing the functionality of this `login` route
+
 router.post('/login', async (req, res) => {
   try {
-    // we search the DB for a user with the provided email
+    // Find the user who matches the posted e-mail address
     const userData = await User.findOne({ where: { email: req.body.email } });
+
     if (!userData) {
-      // the error message shouldn't specify if the login failed because of wrong email or password
-      res.status(404).json({ message: 'Login failed. Please try again!' });
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-    // use `bcrypt.compare()` to compare the provided password and the hashed password
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      userData.password
-    );
-    // if they do not match, return error message
+
+    // Verify the posted password with the password store in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+
     if (!validPassword) {
-      res.status(400).json({ message: 'Login failed. Please try again!' });
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-    // if they do match, return success message
-    res.status(200).json({ message: 'You are now logged in!' });
+
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // Remove the session variables
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
